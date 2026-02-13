fb1be10447ae59e77870ad587bfcd3a38d069d78
Raechel <raechelkelishiya@gmail.com>
2026-02-13 21:32:01 +0530

    fixed allowed hosts

--- myvalentine/settings.py
+++ myvalentine/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4as!9*jk#a1^70$u+$%t)l!r3vzs1edc(w%d58&_q244s8a91a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["myvalentine-n4yx.onrender.com"]
+
 
 
 
--- myvalentine/wsgi.py
+++ myvalentine/wsgi.py
@@ -6,15 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
-
-import sys
 import os
-
-path = '/home/raechel/myvalentine'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myvalentine.settings'
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myvalentine.settings')
+
 application = get_wsgi_application()
